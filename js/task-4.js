cd1b46456c45b40ebf708f0164d6f522e81d0fbc
Andrey389 <148715628+Andrey389@users.noreply.github.com>
2024-03-03 18:00:58 +0200

    add goit-js-hw-07

---
+++ js/task-4.js
@@ -0,0 +1,19 @@
+const form = document.querySelector('.login-form');
+
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const elements = event.currentTarget.elements;
+  if (!elements.email.value || !elements.password.value) {
+    alert('All form fields must be filled in');
+  } else {
+    const info = {
+      email: elements.email.value,
+      password: elements.password.value,
+    };
+    console.log(info);
+  }
+
+  event.currentTarget.reset();
+}
